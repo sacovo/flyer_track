e9a633ba17a0b7a0313d72cbbd8fb52e77008348
Sandro Covo <sandro@sandrocovo.ch>
2021-08-20 10:52:26 +0200

    fix strings

--- map/models.py
+++ map/models.py
@@ -1,16 +1,17 @@
 from django.contrib.auth import get_user_model
 from django.contrib.gis.db import models
 from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
 
 
 class FlyerPatch(models.Model):
-    owner = models.ForeignKey(get_user_model(), models.CASCADE)
-    shape = models.PolygonField()
+    owner = models.ForeignKey(get_user_model(), models.CASCADE, verbose_name=_("Eigentümer*in"))
+    shape = models.PolygonField(verbose_name=_("Bereich"))
 
-    flyer_count = models.IntegerField()
-    date = models.DateTimeField(default=timezone.now)
+    flyer_count = models.IntegerField(verbose_name=_("Anzahl Bierdeckel"))
+    date = models.DateTimeField(default=timezone.now, verbose_name=_("Datum"))
 
     def __str__(self):
         return f'Patch: [{",".join(str(x) for x in self.shape.envelope.extent)}]'
