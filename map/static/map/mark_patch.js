e2544a1948745b154fb51ff49f03693ad634a93c
Sandro Covo <sandro@sandrocovo.ch>
2021-08-23 14:33:50 +0200

    add tooltip to back btn

--- map/static/map/mark_patch.js
+++ map/static/map/mark_patch.js
@@ -7,7 +7,36 @@ map.on("click", function (e) {
   updateShapeField();
 });
 
+let tooltipShown = false;
+
+function showTooltip() {
+  if (!tooltipShown) {
+    const tooltip = document.createElement("div");
+    tooltip.innerHTML = i18n.backTooltip;
+    tooltip.id = "backBtnTooltip";
+    tooltip.classList.add("btn-tooltip");
+    backBtn.parentNode.appendChild(tooltip);
+    tooltipShown = true;
+  }
+}
+
+function hideTooltip() {
+  if (tooltipShown) {
+    document.getElementById("backBtnTooltip").remove();
+    tooltipShown = false;
+  }
+}
+
 const updateShapeField = () => {
+  if (
+    newLayer.getLatLngs()[0].length === 1 &&
+    !tooltipShown &&
+    !(window.localStorage.getItem("tooltipShown") || false)
+  ) {
+    showTooltip();
+  } else if (newLayer.getLatLngs()[0].length === 0 && tooltipShown) {
+    hideTooltip();
+  }
   id_shape.value = JSON.stringify(
     L.polygon(
       newLayer.getLatLngs()[0].map((p) => {
@@ -18,11 +47,19 @@ const updateShapeField = () => {
   );
 };
 
-L.easyButton("fa-undo", function (btn, map) {
-  const points = newLayer.getLatLngs()[0];
-  const length = points.length;
-  if (length > 0) {
-    newLayer.setLatLngs(points.slice(0, length - 1));
-    updateShapeField();
-  }
-}).addTo(map);
+L.easyButton(
+  "fa-undo",
+  function (btn, map) {
+    const points = newLayer.getLatLngs()[0];
+    const length = points.length;
+
+    hideTooltip();
+    if (length > 0) {
+      newLayer.setLatLngs(points.slice(0, length - 1));
+      updateShapeField();
+      window.localStorage.setItem("tooltipShown", true);
+    }
+  },
+  "Back",
+  "backBtn"
+).addTo(map);
